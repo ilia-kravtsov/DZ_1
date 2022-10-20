ee3a15cb66067ad6faba1e3e26b13242c777cd46
ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-10-20 13:20:26 +0300

    Update Clock.tsx

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -27,8 +27,8 @@ function Clock() {
 
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01_hello_tests:02:03)/(23:02:03)/(24:00:00)/(00:00:01_hello_tests) // пишут студенты
+    const stringDate = 'date->date' || <br/> // день.месяц.год (01_hello_tests.02.2022) // пишут студенты, варианты 01_hello_tests.02.0123/01_hello_tests.02.-123/01_hello_tests.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = 'date->day' || <br/> // пишут студенты
